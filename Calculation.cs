c406f3e8ca5ae193b32157d1f75e04cb5a66c30f
Matheesh Suresh <M.Suresh@hottgenroth.de>
2023-12-04 15:08:50 +0100

    Projektdateien hinzufügen.

---
+++ Calculation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Taschenrechner
+{
+    internal class Calculation
+    {
+        public static double Calculate(string function)
+        {
+            CultureInfo culture = CultureInfo.InvariantCulture; // ist dafür das der punkt als dezimalzahl anerkannt wird.
+
+            function = function.Replace(',','.'); // aendert Kommas in Punkt damit man dezimalzahlen verweden kann.
+
+            string[] elements = function.Split(' '); // Teilt die Nummern , Klammern und Operationen in ein array auf
+
+            double result = double.Parse(elements[0], culture);
+
+
+            for (int i = 1; i < elements.Length; i += 2)
+            {
+                string op = elements[i];
+                double nextNumber = double.Parse(elements[i + 1]);
+
+                switch (op)
+                {
+                    case "+":
+                        result += nextNumber;
+                        break;
+                    case "-":
+                        result -= nextNumber;
+                        break;
+                    case "*":
+                        result *= nextNumber;
+                        break;
+                    case "/":
+                        result /= nextNumber;
+                        break;
+                }
+            }
+                return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathVariable mathVariable = new MathVariable();
+
+            Console.WriteLine("Willkommen beim Taschenrechner von Matheesh");
+
+            do
+            {
+                Console.WriteLine("Geben Sie Ihre Mathematische Gleichung ein (mit Leerzeichen):");
+                mathVariable.function = Console.ReadLine();
+
+                if (!mathVariable.function.Contains(" ")) // Wenn kein leerzeichen verwendet wird kommt die fehlermeldung
+                {
+                    Console.WriteLine("Fehler: Bitte geben Sie die Gleichung mit Leerzeichen ein.");
+                }
+
+            } while (!mathVariable.function.Contains(" ")); // Laest die eingabe solange wiederholen bis es mit leerzeichen geschrieben wurde
+
+            double result = Calculation.Calculate(mathVariable.function);
+
+            Console.Clear();
+            Console.WriteLine($"Ihre Gleichung lautet: {mathVariable.function}");
+
+            Console.WriteLine($"Das Ergebnis lautet:{result}");
+            Console.ReadLine();
+        }
+    }
+}
+
