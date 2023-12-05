e9fd24ff85ea961e4a7f861d292fa342e8bb28bd
Matheesh Suresh <M.Suresh@hottgenroth.de>
2023-12-05 12:55:41 +0100

    Fertiger Taschenrechner

--- Calculation.cs
+++ Calculation.cs
@@ -7,37 +7,52 @@ namespace Taschenrechner
     {
         public static double Calculate(string function)
         {
-            CultureInfo culture = CultureInfo.InvariantCulture; // ist dafür das der punkt als dezimalzahl anerkannt wird.
+            CultureInfo culture = CultureInfo.InvariantCulture; // ist dafür da dass der Punkt als Dezimalzahl anerkannt wird.
 
-            function = function.Replace(',', '.'); // aendert Kommas in Punkt damit man dezimalzahlen verweden kann.
+            function = function.Replace(',', '.'); // ändert Kommas in Punkte damit Dezimalzahlen verwendet werden können.
 
-            string[] elements = function.Split(' '); // Teilt die Nummern , Klammern und Operationen in ein array auf
+            // Teilt die Zahlen, Klammern und Operationen in ein Array auf
+            string[] elements = function.Split(' ');
 
-            double result = double.Parse(elements[0], culture);
+            // Punkt vor Strich Rechnung
+            for (int i = 1; i < elements.Length; i += 2)
+            {
+                string op = elements[i];
 
+                if (op == "*" || op == "/")
+                {
+                    double leftOperand = double.Parse(elements[i - 1], culture);
+                    double rightOperand = double.Parse(elements[i + 1], culture);
+
+                    // Führe die entsprechende Operation durch
+                    double result = (op == "*") ? leftOperand * rightOperand : leftOperand / rightOperand;
+
+                    // Aktualisiere der Zahl im Array 
+                    elements[i - 1] = result.ToString(culture);
+                }
+            }
+
+            // Hier wird nach der Punkt vor Strich rechnung der rest ausgerechnet
+            double finalResult = double.Parse(elements[0], culture);
 
             for (int i = 1; i < elements.Length; i += 2)
             {
                 string op = elements[i];
-                double nextNumber = double.Parse(elements[i + 1]);
+                double nextNumber = double.Parse(elements[i + 1], culture);
 
                 switch (op)
                 {
                     case "+":
-                        result += nextNumber;
+                        finalResult += nextNumber;
                         break;
                     case "-":
-                        result -= nextNumber;
-                        break;
-                    case "*":
-                        result *= nextNumber;
-                        break;
-                    case "/":
-                        result /= nextNumber;
+                        finalResult -= nextNumber;
                         break;
                 }
             }
-            return result;
+
+            return finalResult;
         }
     }
 }
+
